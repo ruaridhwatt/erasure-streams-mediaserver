d2f034a86d8256ef72f61ea282a338a68788f7d4
dv11ann <dv11ann@cs.umu.se>
2015-12-05 15:54:09 +0100

    Still haven't got the sending of files to work. You can try to download a file with your webclient through the DL_Slice protocol. Just send anything after the connection is established and it will send u a file of your chosing. just have a file called movie.mp4 in the src folder. If you upload a movie it should now be stored as movie5.mp4 in the src folder. a bit hardcoded at the moment, there are no c-json libraries at the school labs atm

--- src/ErasureServer.c
+++ src/ErasureServer.c
@@ -1,6 +1,9 @@
 /*
  ============================================================================
  Name        : ErasureServer.c
+/*
+ ============================================================================
+ Name        : ErasureServer.c
  Author      : dv11ann, dv12rwt
  Version     : 0.0
  Copyright   : Your copyright notice
@@ -14,8 +17,82 @@
 #include <signal.h>
 #include <libwebsockets.h>
 
+#define PAYLOAD 1024*1024
+
 static volatile int force_exit = 0;
 static struct libwebsocket_context *context;
+
+/*
+ * The video name is hardcoded for now
+ *
+ */
+void receiveFile(void *in, size_t len) {
+	FILE *received_file = fopen("movie5.mp4", "a+b");
+	if (received_file == NULL)
+	{
+		fprintf(stderr, "Failed to open file movie5.mp4\n");
+		exit(EXIT_FAILURE);
+	}
+	fwrite(in, sizeof(char), len, received_file);
+   fclose(received_file);
+}
+
+int send_message(unsigned char* message, int length, int write_mode, struct libwebsocket *wsi) {
+	if (wsi != NULL) {
+		int paddingSize = LWS_SEND_BUFFER_PRE_PADDING + LWS_SEND_BUFFER_POST_PADDING;
+		long msgSize = length + 1;
+		unsigned char *buff = malloc(paddingSize + msgSize);
+		memcpy((void *) &(buff[LWS_SEND_BUFFER_PRE_PADDING]), (void *) message, msgSize);
+		libwebsocket_write(wsi, &buff[LWS_SEND_BUFFER_PRE_PADDING], length , write_mode);
+		printf("%d\n", length);
+		free(buff);
+	}
+	return 0;
+}
+
+/*
+ * Slices up a file into pieces of size determined by PAYLOAD. Sends each piece to a given wsi with function send_message. Must be called by a writeable_callback
+ *  as defined by libwebsockets.readme.
+ *
+ */
+void readFileBytes(struct libwebsocket *wsi, char *filename) {
+	FILE *fileptr;
+	unsigned char *buffer;
+	long filelen;
+
+	fileptr = fopen(filename, "rb");
+	fseek(fileptr, 0, SEEK_END);
+	filelen = ftell(fileptr);
+	rewind(fileptr);
+
+	buffer = (unsigned char *)malloc((filelen+1)*sizeof(unsigned char));
+	fread(buffer, 1, filelen, fileptr);
+
+	unsigned char *buf = (unsigned char *)malloc((PAYLOAD - 4)*sizeof(unsigned char));
+
+	if(filelen < PAYLOAD-5) {
+		send_message(buf, filelen, LWS_WRITE_BINARY, wsi);
+	} else {
+
+		int remain = filelen;
+		int flag = LWS_WRITE_BINARY | LWS_WRITE_NO_FIN;
+		while (remain)
+		{
+			long toCpy = remain > PAYLOAD-5 ? PAYLOAD-5 : remain;
+			memcpy(buf, buffer, toCpy);
+			buffer += toCpy;
+			remain -= toCpy;
+
+			send_message(buf, toCpy, flag, wsi);
+			if(remain < 1) {
+				flag = LWS_WRITE_CONTINUATION;
+			} else {
+				flag = LWS_WRITE_CONTINUATION | LWS_WRITE_NO_FIN;
+			}
+		}
+	}
+	fclose(fileptr);
+}
 
 static int callback_init(struct libwebsocket_context * ctx, struct libwebsocket *wsi, enum libwebsocket_callback_reasons reason, void *user, void *in,
 		size_t len) {
@@ -31,8 +108,9 @@ static int callback_init(struct libwebsocket_context * ctx, struct libwebsocket 
 		printf((char *) in);
 		break;
 
-	case LWS_CALLBACK_RECEIVE:
+	case LWS_CALLBACK_RECEIVE: //stores the received file as "movie.mp4" in src folder.
 		printf("%d\n", (int) len);
+        receiveFile(in, len);
 		break;
 
 	case LWS_CALLBACK_CLOSED:
@@ -44,7 +122,36 @@ static int callback_init(struct libwebsocket_context * ctx, struct libwebsocket 
 	return 0;
 }
 
-static struct libwebsocket_protocols protocols[] = { { "init", callback_init, 0, 10*1024*1024, 0 }, { NULL, NULL, 0 } };
+static int callback_DL_slice(struct libwebsocket_context *ctx, struct libwebsocket *wsi, enum libwebsocket_callback_reasons reason,
+		void *user, void *in, size_t len) {
+
+	switch (reason) {
+
+	case LWS_CALLBACK_FILTER_PROTOCOL_CONNECTION:
+		break;
+	case LWS_CALLBACK_ESTABLISHED:
+		printf("Client connection established\n");
+		break;
+	case LWS_CALLBACK_RECEIVE:
+		printf("message received, sending movie.mp4\n");
+		libwebsocket_callback_on_writable(ctx, wsi);
+		break;
+	case LWS_CALLBACK_CLOSED:
+		break;
+	case LWS_CALLBACK_HTTP:
+		break;
+    case LWS_CALLBACK_SERVER_WRITEABLE:
+    	//For now, hardcode the videon you want to send below.
+    	readFileBytes(wsi, "movie.mp4");
+        printf("Sending video data from file\n");
+    	break;
+	default:
+		break;
+	}
+	return 0;
+}
+
+static struct libwebsocket_protocols protocols[] = { { "init", callback_init, 0, PAYLOAD, 0 },{"DL_Slice", callback_DL_slice, 0 , PAYLOAD}, { NULL, NULL, 0 } };
 
 void sighandler(int sig) {
 	force_exit = 1;
