826b93778ac09c6e726ec550d629cdcdbed2b140
Ruaridh Watt <ruaridh.watt@gmail.com>
2016-01-11 11:57:47 +0100

    script tweaks

--- scripts/envSet.sh
+++ scripts/envSet.sh
@@ -3,4 +3,5 @@
 export BENTO4_HOME=~/Bento4/
 export SCRIPT_HOME=~/git/erasure-streams-server/scripts/
 export CRS_HOME=~/git/crs-erasure-codes/
-export VIDEO_DIR=~/erasurevids/
+mkdir -p ~/erasurevids/$1
+export VIDEO_DIR=~/erasurevids/$1
